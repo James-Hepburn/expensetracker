4c8f11b8bacfedc768006f18d1e539c25be6f11e
James Hepburn <jameshepburn@MacBookPro.lan>
2025-08-06 12:16:18 -0600

    Expense class

--- src/main/java/com/example/expense/tracker/model/Expense.java
+++ src/main/java/com/example/expense/tracker/model/Expense.java
@@ -1,4 +1,32 @@
 package com.example.expense.tracker.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private double amount;
+    private LocalDate date;
+
+    public Expense (String description, double amount) {
+        this.description = description;
+        this.amount = amount;
+        this.date = LocalDate.now ();
+    }
+
+    public String toString () {
+        return "Expense: " + this.description + " Amount: $" + this.amount + " Date: " + date.toString ();
+    }
+
+    public void setDescription (String description) {
+        this.description = description;
+    }
+    public void setAmount (double amount) {
+        this.amount = amount;
+    }
 }
--- src/main/java/com/example/expense/tracker/model/User.java
+++ src/main/java/com/example/expense/tracker/model/User.java
@@ -1,7 +1,5 @@
 package com.example.expense.tracker.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.*;
 
 @Entity
